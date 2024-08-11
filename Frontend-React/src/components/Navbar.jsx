ee03fdb8adf6e522be7cadc111120c453fefb09e
Adedayoke <adedayoke2006@gmail.com>
2024-08-11 15:11:45 +0100

    prettier & tailwindcss setup

---
+++ Frontend-React/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav>
+        <div>Logo</div>
+        <ul>
+            <li>Signup</li>
+            <li>Login</li>
+        </ul>
+        <ul>
+            <li>Habeeb</li>
+            <li>Logout</li>
+        </ul>
+    </nav>
+  )
+}
