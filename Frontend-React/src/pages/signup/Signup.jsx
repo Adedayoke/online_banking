91c99d56cc0741b73a466b8732301a8051452bf0
Adedayoke <adedayoke2006@gmail.com>
2024-08-11 20:25:44 +0100

    signup_one_modification

--- Frontend-React/src/pages/signup/Signup.jsx
+++ Frontend-React/src/pages/signup/Signup.jsx
@@ -4,9 +4,9 @@ import { Outlet, useLocation } from "react-router-dom";
 export default function Signup() {
   const { pathname } = useLocation();
   return (
-    <div className="flex items-center justify-center h-4/5 p-4 bg-stone-200">
-      <div className=" bg-stone-100 rounded shadow-md h-full w-4/5 md:w-1/2 lg:w-1/3" >
-        <p className="text-center text-xl font-bold">Step {pathname === "/signup/one" ? 1 : 2} of 2</p>
+    <div className="flex items-center justify-center h-5/6 p-4 bg-stone-200">
+      <div className=" bg-stone-100 rounded shadow-md h-fit w-4/5 md:w-1/2 lg:w-1/3" >
+        <p className="text-center text-xl font-bold p-4">Step {pathname === "/signup/one" ? 1 : 2} of 2</p>
         <Outlet />
       </div>
     </div>
